9baadcef94162b835b6d06ae2c225bf29e665ef0
Philip Dukhov <philip.dukhov@gmail.com>
2020-09-30 19:23:23 +0700

    scene delegate removed

--- iosApp/Well/Supporting files/AppDelegate.swift
+++ iosApp/Well/Supporting files/AppDelegate.swift
@@ -7,16 +7,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        if #available(iOS 13.0, *) { } else {
-            window = Self.initializeWindow()
-        }
+        window = Self.initializeWindow()
         initializeLogging()
         return true
-    }
-    
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
     
     static func initializeWindow() -> UIWindow {
