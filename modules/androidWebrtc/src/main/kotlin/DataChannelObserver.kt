e0f980e8442a414e595ca788a191bf6965b6b66b
Philip Dukhov <philip.dukhov@gmail.com>
2022-02-22 19:52:28 +0700

    kt files moved to kotlin dir

---
+++ modules/androidWebrtc/src/main/kotlin/DataChannelObserver.kt
@@ -0,0 +1,27 @@
+package com.well.modules.androidWebrtc
+
+import io.github.aakira.napier.Napier
+import org.webrtc.DataChannel
+
+internal open class DataChannelObserver(
+    private val dataChannel: DataChannel,
+    name: String,
+): DataChannel.Observer {
+    private val tag = "${this.javaClass.simpleName} $name"
+
+    override fun onBufferedAmountChange(p0: Long) {
+        Napier.d("onBufferedAmountChange $p0", tag = tag)
+    }
+
+    override fun onStateChange() {
+        onStateChange(dataChannel.state())
+    }
+
+    open fun onStateChange(state: DataChannel.State) {
+        Napier.d("onStateChange $state", tag = tag)
+    }
+
+    override fun onMessage(p0: DataChannel.Buffer?) {
+        Napier.d("onMessage $p0", tag = tag)
+    }
+}
---
+++ modules/utils/viewUtils/src/iosMain/kotlin/NSErrorThrowable.kt
@@ -0,0 +1,19 @@
+package com.well.modules.utils.viewUtils
+
+import platform.Foundation.NSError
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resumeWithException
+
+private const val throwableUserInfoKey = "throwableUserInfoKey"
+
+fun NSError.toThrowable(): Throwable =
+    (userInfo[throwableUserInfoKey] as? Throwable)
+        ?: Throwable(localizedDescription)
+
+// used from iOS part
+@Suppress("unused")
+fun Throwable.toNSError(): NSError =
+    NSError(domain = "com.well.modules.utils", code = 0, userInfo = mapOf(throwableUserInfoKey to this))
+
+fun <T> Continuation<T>.resumeWithError(error: NSError) =
+    resumeWithException(error.toThrowable())
