9172385732fe303be7a9be9e6090ec09a1abfaf0
Philip Dukhov <philip.dukhov@gmail.com>
2022-05-06 14:57:32 +0700

    ws testing code removed

--- modules/networking/src/androidMain/kotlin/webSocketManager/WebSocketClientExt.kt
+++ modules/networking/src/androidMain/kotlin/webSocketManager/WebSocketClientExt.kt
@@ -9,19 +9,11 @@ internal actual suspend fun WebSocketClient.ws(
     block: suspend WebSocketSession.() -> Unit,
 ) {
     client.ws(path, request = {
-//        url.protocol = config.webSocketProtocol
         url.protocol = URLProtocol.WS
     }) {
-//    client.ws(
-//        urlString = "ws://dukhovwellserver-new.com:8090/$path",
-//    ) {
-        try {
-            val session = this
-            coroutineScope {
-                block(WebSocketSession(session, this))
-            }
-        } catch (t: Throwable) {
-            throw t
+        val session = this
+        coroutineScope {
+            block(WebSocketSession(session, this))
         }
     }
 }
